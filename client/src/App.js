9af2e823b19242528c9a328a341eff95dcce4945
Shae Haggis <shae.haggis@gmail.com>
2022-08-07 11:41:14 +0930

    initialised project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+import TestComponent from "./components/TestComponent/TestComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <p>APP WORKS!!</p>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
