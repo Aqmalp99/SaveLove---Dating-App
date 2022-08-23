0c8ef8e7ac21cbaedc3f8591da66be81ec54c2d3
Aqmal Pulle <a1720825@student.adelaide.edu.au>
2022-08-23 19:53:07 +0930

    Google map API integrated with geolocation services.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import "./index.css";
 import TestComponent from "./components/TestComponent/TestComponent";
+import MapLoader from "./components/Map/MapLoader";
 
 function App() {
   return (
     <div className="App">
       <p>APP WORKS!!</p>
-      <TestComponent />
+      <MapLoader/>
     </div>
   );
 }
