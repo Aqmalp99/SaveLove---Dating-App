2dd5a1bcc561806213e41fb6a5a51a01cf302f05
Phu N <tienesphus@gmail.com>
2022-08-29 16:02:47 +0930

    Signup form - No logic yet

---
+++ client/src/components/Signup/SignupLogic.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import SignupForm from './SignupForm';
+
+function SignupLogic() {
+  return (
+    <div className="App">
+      <SignupForm/>
+    </div>
+  );
+}
+
+export default SignupLogic;
