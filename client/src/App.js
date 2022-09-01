8aff161addb8b6aa8764bab14f873fe9646442d4
Chloe-Colette <cloclo.chevailler@gmail.com>
2022-09-01 16:13:33 +0930

    Changes to be committed:
        Video background with 2 people dating with a mask, to remember the covid feature
    
    	modified:   client/src/App.js
    	modified:   client/src/components/Button.css
    	new file:   client/src/components/Footer.css
    	new file:   client/src/components/Footer.js
        new file:   client/src/components/HeroSection.css
    	new file:   client/src/components/HeroSection.js
    	new file:   client/src/components/HomePage/Home.js
    	modified:   client/src/components/Navbar.css
    	new file:   client/src/components/videos/background.mp4
    	renamed:    client/public/images/img-2.jpg -> client/src/images/img-2.jpg

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MapLoader from "./components/Map/MapLoader";
+import MapLoader from './components/Map/MapLoader';
+import Home from './components/HomePage/Home';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <>
       <Router>
+
         <Navbar />
+        <HeroSection />
         <Routes>
-          <Route path='/' exact  />
-          <Route path="/map" element={<MapLoader />} />
+
+        <Route path='/' exact  />
+          <Route path='/map' element={<MapLoader />} />
 
         </Routes>
       </Router>
---
+++ client/src/components/HeroSection.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src={require('.//videos/background.mp4')}autoPlay loop muted />
+      <h3>LOVE AWAITS</h3>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
