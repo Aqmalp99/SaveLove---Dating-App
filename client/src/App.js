ffea56f3df84f9116188a54a986e98bb029ca522
Chloe-Colette <cloclo.chevailler@gmail.com>
2022-09-03 13:39:28 +0930

    On branch issues32&29  Changes to be committed:  Add a footer  issues32 	modified:   client/src/App.css 	modified:   client/src/App.js 	modified:   client/src/components/Footer.css 	modified:   client/src/components/Footer.js 	renamed:    client/src/components/HomePage/Home.js -> client/src/components/Pages/Home.js 	new file:   client/src/components/Pages/MapPage.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MapLoader from './components/Map/MapLoader';
-import Home from './components/HomePage/Home';
+import Home from './components/Pages/Home';
 import HeroSection from './components/HeroSection';
 import MessagingPage from './components/Messaging/MessagingPage';
+import Footer from './components/Footer';
+import MapPage from './components/Pages/MapPage';
 
 function App() {
   return (
@@ -13,14 +15,15 @@ function App() {
       <Router>
 
         <Navbar />
-        <HeroSection />
         <Routes>
 
-          <Route path='/' exact  />
+          <Route path='/' element={<Home/>}></Route>
           <Route path='/chat' element={<MessagingPage />}/>
-          <Route path='/map' element={<MapLoader />} />
+          <Route path='/map' element={<MapPage/>} />
 
         </Routes>
+        <Footer />
+
       </Router>
     </>
   );
---
+++ client/src/components/Pages/MapPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import MapLoader from '../Map/MapLoader';
+
+function MapPage (){
+    return (
+        
+        <>
+            <MapLoader />
+        </>
+    );
+}
+
+export default MapPage;
